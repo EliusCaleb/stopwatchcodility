6a0e73703f3075730495e3055f007527cd38398e
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-08-16 17:47:33 +0300

    codingtest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React,{useState,useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [isRun,setIsRun]=useState(false)
+  const [time,setTime] = useState(0)
+  const timeRef = useRef(null)
+  const startTime =()=>{
+      if(!isRun){
+        setIsRun(true)
+        timeRef.current = setInterval(()=>{
+           setTime(prevTime =>prevTime+1)
+        },1000)
+      }
+  }
+  const pauseTime =()=>{
+      setIsRun(false)
+      clearInterval(timeRef.current)
+  }
+  const resetTime =()=>{
+    setIsRun(false)
+    clearInterval(timeRef.current)
+    setTime(0)
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+       <h2>
+         StopWatch
+       </h2>
+       <h3>{time}</h3>
+       {
+        (
+          !isRun ? (
+            <button onClick={startTime}>Start</button>
+          ) :(
+            <button onClick={pauseTime}>Pause</button>
+          )
+        )
+       }
+       <button onClick={resetTime}> Reset</button>
+      </div>
     </div>
   );
 }
